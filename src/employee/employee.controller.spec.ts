1fb9f4ea87cb18f8843fe3f2a583bd60ce2a9dfd
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-08-02 02:02:36 +0300

    add simple tests for employee

---
+++ src/employee/employee.controller.spec.ts
@@ -0,0 +1,79 @@
+import { EmployeeController } from './employee.controller';
+import { Repository } from 'typeorm';
+import { StaffMember } from '../base/entities/staff-member';
+import { Test, TestingModule } from '@nestjs/testing';
+import { EmployeeService } from './employee.service';
+import { StaffService } from '../base/staff.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateStaffDto } from '../base/dto/create-staff.dto';
+import { StaffTypes } from '../base/constants';
+
+describe('EmployeeController', () => {
+  let controller: EmployeeController;
+  let service: EmployeeService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [EmployeeController],
+      providers: [
+        EmployeeService,
+        StaffService,
+        {
+          provide: getRepositoryToken(StaffMember),
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<EmployeeController>(EmployeeController);
+    service = module.get<EmployeeService>(EmployeeService);
+  });
+
+  it('should successfully create a new employee', async () => {
+    const createEmployeeDto: CreateStaffDto = {
+      name: 'John Doe',
+      joinDate: new Date(),
+      baseSalary: 1000,
+      position: StaffTypes.EMPLOYEE,
+    };
+
+    const newEmployee = new StaffMember();
+    newEmployee.id = 1;
+    newEmployee.name = createEmployeeDto.name;
+    newEmployee.joinDate = createEmployeeDto.joinDate;
+    newEmployee.baseSalary = createEmployeeDto.baseSalary;
+    newEmployee.position = createEmployeeDto.position;
+
+    jest.spyOn(service, 'create').mockResolvedValue(newEmployee);
+
+    const result = await controller.create(createEmployeeDto);
+    expect(result).toEqual(newEmployee);
+  });
+
+  it('should successfully calculates a salary for one employee', async () => {
+    const createEmployeeDto: CreateStaffDto = {
+      name: 'John Doe',
+      joinDate: new Date(2024, 7, 1),
+      baseSalary: 1000,
+      position: StaffTypes.EMPLOYEE,
+    };
+
+    const newEmployee = new StaffMember();
+    newEmployee.id = 1;
+    newEmployee.name = createEmployeeDto.name;
+    newEmployee.joinDate = createEmployeeDto.joinDate;
+    newEmployee.baseSalary = createEmployeeDto.baseSalary;
+    newEmployee.position = createEmployeeDto.position;
+
+    jest.spyOn(service, 'create').mockResolvedValue(newEmployee);
+    jest.spyOn(service, 'findOne').mockResolvedValue(newEmployee);
+    const result = await controller.create(createEmployeeDto);
+    const { salary } = await controller.calculateSalary(result.id, {
+      date: '2026-08-01',
+    });
+    expect(salary).toBe(1060);
+  });
+});
--- src/employee/employee.service.spec.ts
+++ src/employee/employee.service.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeService } from './employee.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { StaffMember } from '../base/entities/staff-member';
+import { Repository } from 'typeorm';
+import { StaffTypes } from '../base/constants';
+import { StaffService } from '../base/staff.service';
 
 describe('EmployeeService', () => {
   let service: EmployeeService;
+  let staffRepository: Repository<StaffMember>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmployeeService],
+      providers: [
+        EmployeeService,
+        StaffService,
+        {
+          provide: getRepositoryToken(StaffMember),
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<EmployeeService>(EmployeeService);
+    staffRepository = module.get<Repository<StaffMember>>(
+      getRepositoryToken(StaffMember),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should calculate salary correctly', async () => {
+    const mockEmployee1 = new StaffMember();
+    mockEmployee1.name = 'John Doe';
+    mockEmployee1.joinDate = new Date(2020, 0, 1);
+    mockEmployee1.baseSalary = 550;
+    mockEmployee1.position = StaffTypes.EMPLOYEE;
+
+    jest.spyOn(staffRepository, 'findOne').mockResolvedValue(mockEmployee1);
+    jest.spyOn(staffRepository, 'save').mockResolvedValue(mockEmployee1);
+
+    const salary = service.calculateSalary(
+      mockEmployee1.id,
+      new Date(2026, 0, 1),
+    );
+    salary.then((v) => {
+      expect(v).toBe(649);
+    });
   });
 });
