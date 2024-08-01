ce9673238a835f16e22e2ea58fd6de67a4fcfac3
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-08-01 19:12:21 +0300

    moves salary values to constants.ts

--- src/base/constants.ts
+++ src/base/constants.ts
@@ -1,8 +1,19 @@
-export const STAFF_STORAGE = [];
-
 export enum StaffTypes {
   EMPLOYEE = 'employee',
   MANAGER = 'manager',
-  SALES = 'sales'
+  SALES = 'sales',
 }
 
+//Employees
+export const EMPLOYEE_MAX_SALARY_INCREASE: number = 0.3;
+export const EMPLOYEE_YEAR_SALARY_INCREASE: number = 0.03;
+
+//Managers
+export const MANAGER_MAX_SALARY_INCREASE: number = 0.4;
+export const MANAGER_YEAR_SALARY_INCREASE: number = 0.05;
+export const MANAGER_SUBORDINATES_SALARY_INCREASE: number = 0.005;
+
+//Sales
+export const SALES_MAX_SALARY_INCREASE: number = 0.35;
+export const SALES_YEAR_SALARY_INCREASE: number = 0.01;
+export const SALES_SUBORDINATES_SALARY_INCREASE: number = 0.003;
