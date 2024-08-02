31e0a682ee85e8a50ceb63aa39a6470faa83723e
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-08-02 17:30:18 +0300

    updates comments

--- src/base/salary.calculator.strategy.ts
+++ src/base/salary.calculator.strategy.ts
@@ -2,9 +2,10 @@ import { StaffMember } from './entities/staff-member';
 
 export abstract class SalaryCalculatorStrategy {
   /**
-   * Calculates salary for staff member.
+   * Calculates salary for staff member on a given date.
    * @param sm staff member.
    * @param date settlement date.
+   * @return calculated salary
    */
   public static calculateSalary(sm: StaffMember, date: Date): number {
     return 0;
